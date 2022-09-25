69843c3f5bb590f694467c6d7d22fc0f0f02ebf0
Nuo <codypham5@gmail.com>
2022-09-25 18:58:51 -0400

    Created functions for starting game, displaying the questions, and setting them.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Containers
 var viewHighscoresEl = document.getElementById("view_hightscores");
-var startingContainer = document.getElementById("starting_container");
-var questionContainer = document.getElementById("question_container");
-var highscoreContainer = document.getElementById("highscore_container");
+var startingContainerEl = document.getElementById("starting_container");
+var questionContainerEl = document.getElementById("question_container");
+var highscoreContainerEl = document.getElementById("highscore_container");
 
 var highscoreListEl = document.getElementById("highscore_list");
-var initials = document.getElementById("initials");
+var initialsEl = document.getElementById("initials");
 var scoreMsgEl = document.getElementById("score_msg");
 var viewHighscoresEl = document.getElementById("view_highscores");
 var questionsEl = document.getElementById("questions");
@@ -23,31 +23,73 @@ var answerButtonsEl = document.getElementById("answer_buttons");
 //Beginning score
 var score = 0;
 
+//Shuffling questions
+var shuffledQuestions = [];
+var questionIndex = 0;
+
 //Array of the quiz questions
 var questions = [
     {
-        q: "Inside which HTML element do we put the JavaScript?",
+        q: 'Inside which HTML element do we put the JavaScript?',
         choices: [{choice: '<javascript>'}, {choice: '<js>'}, {choice: '<scripting>'}, {choice: '<script>'}],
-        a: "<script>"
-    }
+        a: '<script>'
+    },
     {
         q: 'How do you write "Hello World" in an alert box?',
         choices: [{choice: 'msgBox("Hello World");'}, {choice: 'alert("Hello World");'}, {choice: 'msg("Hello World")'}, {choice: 'alertBox("Hello World");'}],
         a: 'alert("Hello World");'
-    }
+    },
     {
         q: 'How can you add a comment in a JavaScript?',
         choices: [{choice: '//This is a comment'}, {choice: '<!--This is a comment-->'}, {choice: "'This is a comment"}, {choice: 'None of the above'}],
         a: '//This is a comment'
-    }
+    },
     {
         q: 'Which event occurs when the user clicks on an HTML element?',
         choices: [{choice: 'onchange'}, {choice: 'onclick'}, {choice: 'onmouseover'}, {choice: 'onmouseclick'}],
         a: 'onclick'
-    }
+    },
     {
         q: 'Which operator is used to assign a value to a variable?',
         choices: [{choice: 'x'}, {choice: '*'}, {choice: '='}, {choice: '-'}],
         a: '='
     }
 ];
+
+var startGame = function() {
+    //Hides the starting screen and starts showing the quiz and questions
+    startingContainerEl.classList.add('hidden');
+    startingContainerEl.classList.remove('shown');
+    questionContainerEl.classList.remove('hidden');
+    questionContainerEl.classList.add('shown');
+
+    console.log("TEST");
+
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+
+    //Starts quiz
+    setQuestion()
+}
+
+var displayQuestion = function(index) {
+    questionsEl.innerText = index.q
+    for (var i = 0; i < index.choices.length; i++) {
+        var answerButton = document.createElement('button')
+        answerButton.innerText = index.choices[i].choice
+        answerButton.classList.add('btn')
+        answerButton.classList.add('answerbtn')
+        answerButton.addEventListener("click", answerCheck)
+        answerButtonsEl.appendChild(answerButton)
+        }
+}
+
+//Checks to see if the answer is correct
+var answerCheck = function() {
+
+}
+
+var setQuestion = function() {
+    displayQuestion(shuffledQuestions[questionIndex]);
+}
+
+btnStartEl.addEventListener("click", startGame);
