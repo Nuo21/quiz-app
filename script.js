ea57c966b2375c5c4aa4d280f7e6ed801829bc4f
Nuo <codypham5@gmail.com>
2022-09-24 21:29:58 -0400

    Added ending container that includes form to record user initials and score. Added some variables to js file.

---
+++ script.js
@@ -0,0 +1,7 @@
+//Containers
+var viewHighscoresEl = document.getElementById("view_hightscores");
+var startingContainer = document.getElementById("starting_container");
+var questionContainer = document.getElementById("question_container");
+//Timer
+var timerEl = document.getElementById("timer");
+timerEl.innerText = 0;
