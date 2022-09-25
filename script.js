36dd13410a94b4d4294a90fa86d872d3f2bcb6da
Nuo <codypham5@gmail.com>
2022-09-24 22:26:50 -0400

    Created variables and elements in js file.

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 var viewHighscoresEl = document.getElementById("view_hightscores");
 var startingContainer = document.getElementById("starting_container");
 var questionContainer = document.getElementById("question_container");
+var highscoreContainer = document.getElementById("highscore_container");
+
+var highscoreListEl = document.getElementById("highscore_list");
+var initials = document.getElementById("initials");
+var scoreMsgEl = document.getElementById("score_msg");
+var viewHighscoresEl = document.getElementById("view_highscores");
+var questionsEl = document.getElementById("questions");
+
 //Timer
 var timerEl = document.getElementById("timer");
 timerEl.innerText = 0;
+
+//Buttons
+var btnStartEl = document.getElementById("start_game");
+var btnClearScoresEl = document.getElementById("clear_highscores");
+var btnGoBackEl = document.getElementById("go_back");
+var answerButtonsEl = document.getElementById("answer_buttons");
